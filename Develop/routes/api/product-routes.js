05d39538996aba19d0cb04eac6e4ef6fbc2af465
Abdulla <default@defaults-iMac.local>
2023-07-14 17:23:56 -0500

    setting up the api route to enable the crud ops

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,16 +4,37 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    // Find all categories and include associated Products
+    const categories = await Category.findAll({
+      include: [Product],
+    });
+    res.json(categories);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
+
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+
+router.get('/:id', async (req, res) => {
+  try {
+    // Find a single category by its `id` and include associated Products
+    const category = await Category.findByPk(req.params.id, {
+      include: [Product],
+    });
+    if (!category) {
+      res.status(404).json({ message: 'Category not found' });
+      return;
+    }
+    res.json(category);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
+
 
 // create new product
 router.post('/', (req, res) => {
@@ -25,27 +46,29 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
-    .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
-          return {
-            product_id: product.id,
-            tag_id,
-          };
+      Product.create(req.body)
+        .then((product) => {
+          // if there are product tags, we need to create pairings to bulk create in the ProductTag model
+          if (req.body.tagIds && req.body.tagIds.length) {
+            const productTagIdArr = req.body.tagIds.map((tag_id) => {
+              return {
+                product_id: product.id,
+                tag_id,
+              };
+            });
+            return ProductTag.bulkCreate(productTagIdArr);
+          }
+          // if no product tags, just respond
+          res.status(200).json(product);
+        })
+        .then((productTagIds) => {
+          res.status(200).json(productTagIds);
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(400).json(err);
         });
-        return ProductTag.bulkCreate(productTagIdArr);
-      }
-      // if no product tags, just respond
-      res.status(200).json(product);
-    })
-    .then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
     });
-});
 
 // update product
 router.put('/:id', (req, res) => {
